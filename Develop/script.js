b1a1f6bbb2292ededb92433ee8b761f6da1c6813
anthonycc <anthonyccardozo@gmail.com>
2021-06-27 12:43:34 -0400

    started adding persistance

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,22 @@
 // display current date
 $("#currentDay").text(moment().format("MMMM Do, YYYY"));
 
-// "current time" variable
 
 
+// current tiem variable
+var currentTime = moment().hour();
 
 // COLOUR-CODING TIMEBLOCKS
 function auditTime() {
     // loop through all the timeblocks
     $(".time-block").each(function() {
-        // check time to see which colour to apply
-        if (thisTime === currentTime) {
+
+        // VAR UNDEFINED
+        var currentBlocksTime // ...
+        // check time to see which colour/class to apply
+        if (currentBLocksTime === currentTime) {
             $(this).addClass(".present");
-        } if (thisTime > currentTime) {
+        } if (currentBlocksTime > currentTime) {
             $(this).addClass(".future");
         } else {
             $(this).addClass(".past");
@@ -20,8 +24,33 @@ function auditTime() {
     })
 }
 
+// run audit function
+auditTime();
 
 
+// save task when save button clicked
+$(".save-Btn").click(function() {
+    
+    // GET task time and description
+    var taskTime = $(this).parent().attr("id");
+    var taskDescription = $(this).siblings(".description").val(); 
+
+    // save tasks & descrips to localstorage
+    localStorage.setItem(taskDescription, taskTime)
+});
+
+
+// show locally stored tasks
+
+$("#time9 .description").text(localStorage.getItem("time9"));
+$("#time10 .description").text(localStorage.getItem("time10"));
+$("#time11 .description").text(localStorage.getItem("time11"));
+$("#time12 .description").text(localStorage.getItem("time12"));
+$("#time1 .description").text(localStorage.getItem("time1"));
+$("#time2 .description").text(localStorage.getItem("time2"));
+$("#time3 .description").text(localStorage.getItem("time3"));
+$("#time4 .description").text(localStorage.getItem("time4"));
+$("#time5 .description").text(localStorage.getItem("time5"));
 
 
 
