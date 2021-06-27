a295de4fac8db7b0ed90771bd5acab6fa8559888
anthonycc <anthonyccardozo@gmail.com>
2021-06-26 23:04:15 -0400

    started audit function for colour codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,24 @@
 // display current date
 $("#currentDay").text(moment().format("MMMM Do, YYYY"));
 
+// "current time" variable
 
+
+
+// COLOUR-CODING TIMEBLOCKS
+function auditTime() {
+    // loop through all the timeblocks
+    $(".time-block").each(function() {
+        // check time to see which colour to apply
+        if (thisTime === currentTime) {
+            $(this).addClass(".present");
+        } if (thisTime > currentTime) {
+            $(this).addClass(".future");
+        } else {
+            $(this).addClass(".past");
+        }
+    })
+}
 
 
 
